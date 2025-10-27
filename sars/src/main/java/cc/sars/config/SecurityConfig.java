e4fb6268891758277d72f32a0560369e5fdbd1eb
macaquinyo <miamigoelmacaco@proton.me>
2025-10-27 10:54:10 +0100

    Añadido proyecto de spring con hola mundo. Closes #15

---
+++ sars/src/main/java/cc/sars/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package cc.sars.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/", "/index", "/index.html", "/hello", "/css/**", "/js/**").permitAll()
+                .anyRequest().authenticated()
+            )
+            .formLogin(Customizer.withDefaults());
+        return http.build();
+    }
+}
